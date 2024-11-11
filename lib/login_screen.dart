e5f94dd2485b8d0d101a6dfc439f9bb6dd694f4c
Ravidu Dilshan <36620171+RavinduDilshan@users.noreply.github.com>
2024-11-11 10:37:28 +0530

    login screen ui

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:instagram_flutter/utils/colors.dart';
+import 'package:instagram_flutter/widgets/text_field_input.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -10,6 +11,16 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +31,10 @@ class _LoginScreenState extends State<LoginScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            Flexible(
+              flex: 2,
+              child: Container(),
+            ),
             //svg image
             SvgPicture.asset(
               'assets/ic_instagram.svg',
@@ -29,9 +44,59 @@ class _LoginScreenState extends State<LoginScreen> {
             ),
             const SizedBox(height: 64),
             //text field input for email
+            TextFieldInput(
+              textEditingController: _emailController,
+              hintText: 'Enter your email',
+              textInputType: TextInputType.emailAddress,
+            ),
+            const SizedBox(height: 24),
             //text field input for password
+            TextFieldInput(
+              textEditingController: _passwordController,
+              hintText: 'Enter your password',
+              textInputType: TextInputType.text,
+              isPass: true,
+            ),
+            const SizedBox(height: 24),
             //button login
+            Container(
+              width: double.infinity,
+              alignment: Alignment.center,
+              padding: const EdgeInsets.symmetric(vertical: 12),
+              decoration: const ShapeDecoration(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(4))),
+                  color: blueColor),
+              child: const Text('Log in'),
+            ),
+            const SizedBox(height: 12),
+            Flexible(
+              flex: 2,
+              child: Container(),
+            ),
             //transition to signing up
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  child: const Text("Don't have an account?"),
+                ),
+                const SizedBox(
+                  width: 3,
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: const Text(
+                      "Sign up",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                )
+              ],
+            )
           ],
         ),
       )),
--- lib/widgets/text_field_input.dart
+++ lib/widgets/text_field_input.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 
 class TextFieldInput extends StatelessWidget {
-  const TextFieldInput({super.key});
+  final TextEditingController textEditingController;
+  final bool isPass;
+  final String hintText;
+  final TextInputType textInputType;
+  const TextFieldInput(
+      {super.key,
+      required this.textEditingController,
+      this.isPass = false,
+      required this.hintText,
+      required this.textInputType});
 
   @override
   Widget build(BuildContext context) {
-    return TextField();
+    final inputBorder =
+        OutlineInputBorder(borderSide: Divider.createBorderSide(context));
+    return TextField(
+      controller: textEditingController,
+      decoration: InputDecoration(
+          hintText: hintText,
+          border: inputBorder,
+          focusedBorder: inputBorder,
+          enabledBorder: inputBorder,
+          filled: true,
+          contentPadding: const EdgeInsets.all(8)),
+      keyboardType: textInputType,
+      obscureText: isPass,
+    );
   }
 }
