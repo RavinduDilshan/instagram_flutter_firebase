16e718898720816dcc0f8e76eaddea226b7b679b
Ravidu Dilshan <36620171+RavinduDilshan@users.noreply.github.com>
2024-11-11 09:49:38 +0530

    instagram logo added to login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/firebase_options.dart';
+import 'package:instagram_flutter/login_screen.dart';
 import 'package:instagram_flutter/responsive/mobile_screen_layout.dart';
 import 'package:instagram_flutter/responsive/responsive_layout_screen.dart';
 import 'package:instagram_flutter/responsive/web_screen_layout.dart';
@@ -24,9 +25,10 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark()
           .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
-      home: const ResponsiveLayout(
+      /*  home: const ResponsiveLayout(
           webScreenLayout: WebScreenLayout(),
-          mobileScreenLayout: MobileScreenLayout()),
+          mobileScreenLayout: MobileScreenLayout()), */
+      home: const LoginScreen(),
     );
   }
 }
