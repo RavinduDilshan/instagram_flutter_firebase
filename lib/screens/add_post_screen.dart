20890c9fc32487d5b201cde8594a42277395af6a
Ravidu Dilshan <36620171+RavinduDilshan@users.noreply.github.com>
2024-12-02 23:34:02 +0530

    add post screen top section done

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -32,6 +32,44 @@ class _AddPostScreenState extends State<AddPostScreen> {
                       fontSize: 16)))
         ],
       ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://images.unsplash.com/photo-1719937206589-d13b6b008196?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8'),
+              ),
+              SizedBox(
+                width: MediaQuery.sizeOf(context).width * 0.45,
+                child: const TextField(
+                  decoration: InputDecoration(
+                      hintText: 'write a caption...', border: InputBorder.none),
+                  maxLines: 8,
+                ),
+              ),
+              SizedBox(
+                width: 45,
+                height: 45,
+                child: AspectRatio(
+                  aspectRatio: 487 / 451,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                'https://images.unsplash.com/photo-1719937206589-d13b6b008196?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8'),
+                            fit: BoxFit.fill,
+                            alignment: FractionalOffset.topCenter)),
+                  ),
+                ),
+              ),
+              const Divider()
+            ],
+          )
+        ],
+      ),
     );
   }
 }
