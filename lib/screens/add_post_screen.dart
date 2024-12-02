92bcd65359c0a2e024dd66f1066842e469b36fac
Ravidu Dilshan <36620171+RavinduDilshan@users.noreply.github.com>
2024-12-02 20:30:15 +0530

    add post screen added

---
+++ lib/screens/add_post_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class AddPostScreen extends StatefulWidget {
+  const AddPostScreen({super.key});
+
+  @override
+  State<AddPostScreen> createState() => _AddPostScreenState();
+}
+
+class _AddPostScreenState extends State<AddPostScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
