f3ed17fca7885a187062ffbc777e02fb18b93554
Ravidu Dilshan <36620171+RavinduDilshan@users.noreply.github.com>
2024-12-02 23:45:07 +0530

    select post image using gallary option added

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -33,6 +33,17 @@ class _AddPostScreenState extends State<AddPostScreen> {
                     _file = file;
                   });
                 },
+              ),
+              SimpleDialogOption(
+                padding: const EdgeInsets.all(20),
+                child: const Text('choose from galary'),
+                onPressed: () async {
+                  Navigator.of(context).pop();
+                  Uint8List file = await pickImage(ImageSource.gallery);
+                  setState(() {
+                    _file = file;
+                  });
+                },
               )
             ],
           );
