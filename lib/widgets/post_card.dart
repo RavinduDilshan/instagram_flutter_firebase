d0538ebbd36b471c616364167ae475fe75aa7837
Ravidu Dilshan <36620171+RavinduDilshan@users.noreply.github.com>
2024-12-04 18:44:15 +0530

    post card widget added

---
+++ lib/widgets/post_card.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:instagram_flutter/utils/colors.dart';
+
+class PostCard extends StatelessWidget {
+  const PostCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: mobileBackgroundColor);
+  }
+}
