b823a95486933cf4ca422364b565bff2fcbd74c6
Ravidu Dilshan <36620171+RavinduDilshan@users.noreply.github.com>
2024-12-08 17:44:22 +0530

    description spacing added

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -131,7 +131,7 @@ class PostCard extends StatelessWidget {
                             style:
                                 const TextStyle(fontWeight: FontWeight.bold)),
                         TextSpan(
-                          text: snap['description'],
+                          text: ' ${snap['description']}',
                         )
                       ])),
                 ),
